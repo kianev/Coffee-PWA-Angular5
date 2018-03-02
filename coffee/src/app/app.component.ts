d705a6dc5db39894df4b59e8cf2b8d0defe63282
Kaloian Ianev <kianev@gmail.com>
2018-03-02 15:22:37 +0200

    when offline make the body style gray with window.addEventListener("offline")

--- coffee/src/app/app.component.ts
+++ coffee/src/app/app.component.ts
@@ -10,7 +10,21 @@ export class AppComponent implements OnInit{
 
   constructor(private snackBar: MatSnackBar) {}
 
+  updateNetworkStatusUI() {
+    if(navigator.onLine){
+      (document.querySelector("body")as any).style = "";
+    } else {
+      (document.querySelector("body")as any).style = "filter: grayscale(1)";
+    }
+  }
+
   ngOnInit() {
+    this.updateNetworkStatusUI();
+    window.addEventListener("online", this.updateNetworkStatusUI);
+    window.addEventListener("offline", this.updateNetworkStatusUI);
+
     this.snackBar.open('You can add this PWA to the Homescreen', '', {duration: 3000});
   }
+
+
 }
